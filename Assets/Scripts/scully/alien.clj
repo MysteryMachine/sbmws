441ea06f59d78d51233701cde4ce67083183ae8d
Sal <physicsfu@gmail.com>
2015-02-06 00:10:31 -0500

    alien motion

--- Assets/Scripts/scully/alien.clj
+++ Assets/Scripts/scully/alien.clj
@@ -8,11 +8,23 @@
          awake
          collide)
 
-(defcomponent Alien [^int health]
+(defcomponent Alien [^int health ^float t]
   (Awake [this] (awake this)) 
   (Update [this] (update this))
   (OnTriggerEnter2D [this collider] (collide this collider)))
 
 (defn awake [this] this)
-(defn update [this] this)
+(defn update [this]
+  (let [t (.t this)
+        new-t (if (> t (float 36000)) (float 0) (+ (float 1) t))
+        radt (/ t Math/PI 2 10)
+        x (* 2 (Math/Sin radt))
+        y (Math/Sin (* 2 radt))
+        transform (.GetComponent this "Transform")
+        z (.z (.localPosition transform))
+        v (Vector3. (float x) (float y) (float z))]
+    (do
+      (set! (.localPosition transform) v)
+      (set! (.t this) (float new-t)))))
+
 (defn collide [this collider] (enemy-take-damage this collider))
